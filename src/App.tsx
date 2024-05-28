f6ee5e7a359aec454f8c2867c85af05be6963d86
Stepanova Vassa <vstepanova@fab.pasona.tech>
2024-05-28 10:29:47 +0900

    some tests

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components/button';
+
+function App() {
+  return (
+    <>
+      <Button>Normal</Button>
+      <Button $primary>Primary</Button>
+    </>
+  );
+}
+
+export default App
